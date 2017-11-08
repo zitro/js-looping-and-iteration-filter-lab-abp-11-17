097ecbaf2f185405e8fc50692fef968da54cd35a
Bryan Ortiz <bryan@bryanortiz.com>
2017-11-08 17:06:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution in this file
+function findMatching(driver, str){
+  for (const user of driver) {
+let newDriver = [user]
+    return newDriver;
+  }
+}
+
+function fuzzyMatch(){
+
+}
+function matchName(){
+
+}
