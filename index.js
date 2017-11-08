322fb0525f19942674c59475c237cbb7820fce33
Bryan Ortiz <bryan@bryanortiz.com>
2017-11-08 17:10:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solution in this file
-function findMatching(driver, str){
-  return driver.filter(function (driver) { return driver; });
+function findMatching(thing, name){
+  return thing.filter(function (driver) { return driver; });
   }
 }
 
