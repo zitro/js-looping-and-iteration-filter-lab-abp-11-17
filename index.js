eebedfe76549e73b0d5d8ef7c31de5ef9f6884b7
Bryan Ortiz <bryan@bryanortiz.com>
2017-11-08 17:58:57 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file
+function findMatching(thing, name){
+  const newThing = [];
+  return thing.filter(function (newThing) {
+    return newThing.toLowerCase() === name.toLowerCase();
+   });
+  }
+
+
+function fuzzyMatch(thing, letters){
+  const thing2 = [];
+  let nameLetters = letters.length;
+  return thing.filter(function (thing2){
+    return thing2.slice(0, nameLetters) === letters;
+  })
+
+}
+function matchName(thing, name){
+  const thing3 = [];
+  return thing.filter(function (thing3) {
+    return thing3.name.toLowerCase() === name.toLowerCase();
+   });
+}
+
+/*This function takes an array of drivers and a string as arguments.
+In this function, each element of the drivers array is a JavaScript
+object that has a property of name. The function should return each
+element whose name property matches the provided string argument.*/
